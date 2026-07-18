08b1643680038033208234f0caf28ee0416c3fe9
Satria Bagus <satriabw97@gmail.com>
2026-07-18 14:04:02 +0200

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        result = [0] * (len(num1) + len(num2))
+        for i in range(len(num1) - 1, -1, -1):
+            for j in range(len(num2) - 1, -1, -1):
+                mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+
+                p1, p2 = i + j, i + j + 1
+                total = mul + result[p2]
+
+                result[p2] = total % 10
+                result[p1] += total // 10
+
+        ans = []
+        for digit in result:
+            if not (len(ans) == 0 and digit == 0):
+                ans.append(str(digit))
+                
+        return "".join(ans)
